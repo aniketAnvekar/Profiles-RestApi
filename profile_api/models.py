c3607c5dd9439ec6055d9b76ceaf38d2eff46b46
aniketAnvekar <aniketanvekar25@gmail.com>
2020-05-30 20:40:26 +0530

    working with views and urls

--- profile_api/models.py
+++ profile_api/models.py
@@ -17,7 +17,7 @@ class UserProfileManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_super_user(self,email,name,password):
+    def create_superuser(self,email,name,password):
         """Create and save new superuser with given details"""
         user = self.create_user(email,name,password)
         user.is_superuser = True
--- profile_api/views.py
+++ profile_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    '''Test api view'''
+
+    def get(self,request,format=None):
+        '''Returns a list of API view features'''
+        an_apiview = [
+                'Is similar to traditional Django view',
+                'Gives you the most control over application logic',
+                'Uses HTTP methods as function (get,post,patch,put,delete)'
+        ]
+        return Response({'message':'hello','an_apiview':an_apiview})
